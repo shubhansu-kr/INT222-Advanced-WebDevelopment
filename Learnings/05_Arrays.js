782206f95f2e08adf55212034ee3a4f7dc5b286f
Shubhansu kumar <shubhansu2021@gmail.com>
2024-02-16 12:54:08 +0530

    Arr

---
+++ Learnings/05_Arrays.js
@@ -0,0 +1,14 @@
+var num1 = [1, 2, 3];
+var num2 = [4, 5, 6];
+var num3 = [3, 2, 1];
+
+// undefined
+console.log(num1.num2);
+
+// concat 2 on 1
+console.log(num1.concat(num2));
+
+// concat 2 and 3 on 1
+console.log(num1.concat(num2, num3));
+
+console.log(num1.join(num2));
