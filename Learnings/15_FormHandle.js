5f8dc7fca54fbd5aac4663ad3a8032c647e15763
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-21 11:40:49 +0530

    Get the form data in get method

--- Learnings/15_FormHandle.js
+++ Learnings/15_FormHandle.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+
 app.get('/submit', (req, res) => {
     resObj = {
         email: req.query.email,
         pass: req.query.password
     }
-    res.send(resObj);
+    // 
+    res.send(JSON.stringify(resObj));
 })
