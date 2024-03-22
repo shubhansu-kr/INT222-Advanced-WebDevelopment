2da944d931308a29468711a100c08315bb3c7c3e
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-22 12:47:31 +0530

    Calculator

--- app/index.js
+++ app/index.js
@@ -6,22 +6,50 @@ const logger = morgan('tiny');
 const app = express();
 app.use(logger);
 
-app.use('/submit', (req, res) => {
+const lg = (req, res, next) => {
+    morgan(':date[iso] :method :url :status :remote-addr');
+    next();
+}
+
+app.use('/submit', lg, (req, res) => {
 
     console.log(req.url);
     resObj = {
         text: req.query.text,
         pass: req.query.password
     }
-    morgan(':date[iso] :method :url :status :remote-addr');
+    
     res.write('<a href="http://localhost:5000/">Home</a>');
     res.write('\n');
     res.write(JSON.stringify(resObj));
     res.end();
 })
 
-app.use('/', (req, res) => {
-    morgan(':date[iso] :method :url :status :remote-addr');
+app.use('/add', lg, (req, res) => {
+    res.write('<a href="http://localhost:5000/calc">Calc</a>');
+    a = req.query.fn;
+    b = req.query.sn;
+
+    a = parseInt(a); 
+    b = parseInt(b);
+
+    sum = a+b;
+    if (isNaN(sum)) {
+        res.write('Enter valid number');
+    }
+    else {
+        res.write('sum of ' + a + " + " + b + " = " + (a+b));
+    }
+
+    res.end();    
+})
+
+app.use('/calc', lg,  (req, res) => {
+    const filePath = path.join(__dirname, 'public/add.html');
+    res.sendFile(filePath);
+})
+
+app.use('/', lg, (req, res) => {
     const filePath = path.join(__dirname, 'public/index.html');
     res.sendFile(filePath);
 })
