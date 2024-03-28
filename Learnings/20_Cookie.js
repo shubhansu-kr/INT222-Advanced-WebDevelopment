975d4e2fea72d76b202fdda57be086cab6f6b422
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-28 11:44:57 +0530

    Cookie

--- Learnings/20_Cookie.js
+++ Learnings/20_Cookie.js
@@ -4,14 +4,23 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
+// Cookies are always sent in the http transfer.
+app.get('/', (req, res) => {
+    // Initail response: Null
+    console.log("Home: ", req.cookies);
+    res.send('Home');
+});
+
 app.get('/setCookie', (req, res)=>{
     res.cookie('key', 'value');
     res.send('Cookie Set in Browser');
-    console.log('Cookies: Set: ' + req.cookies);
+    console.log('Cookies: Set: ', req.cookies);
 });
 
 app.get('/clearCookie', (req, res) => {
-    console.log("Cookies: Clear: " + req.cookies);
-})
+    console.log("Cookies: Clear: ", req.cookies);
+    res.clearCookie('key', 'value');
+    res.send('Cookie Cleared');
+});
 
 app.listen(5050);
