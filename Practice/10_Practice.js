f9e72819d2aedd9c4b13291238a460a6691cb70e
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-28 10:34:58 +0530

    Practice

---
+++ Practice/10_Practice.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const myName = (req, res, next) => {
+    console.log('Middleware Started');
+    next();
+}
+
+app.use(myName);
+
+app.get('/', (req, res) => {
+    res.send('Hello Middleware!');
+})
+
+app.listen(5050);
--- app/index.js
+++ app/index.js
@@ -1,58 +1,15 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const logger = morgan('tiny');
-
+const express = require("express");
 const app = express();
-app.use(logger);
 
-const lg = (req, res, next) => {
-    morgan(':date[iso] :method :url :status :remote-addr');
-    next();
-}
+const myName = (req, res, next) => {
+	console.log("Middleware Started");
+	next();
+};
 
-app.use('/submit', lg, (req, res) => {
+app.use(myName);
 
-    console.log(req.url);
-    resObj = {
-        text: req.query.text,
-        pass: req.query.password
-    }
-    
-    res.write('<a href="http://localhost:5000/">Home</a>');
-    res.write('\n');
-    res.write(JSON.stringify(resObj));
-    res.end();
-})
+app.get("/", (req, res) => {
+	res.send("Hello Middleware!");
+});
 
-app.use('/add', lg, (req, res) => {
-    res.write('<a href="http://localhost:5000/calc">Calc</a>');
-    a = req.query.fn;
-    b = req.query.sn;
-
-    a = parseInt(a); 
-    b = parseInt(b);
-
-    sum = a+b;
-    if (isNaN(sum)) {
-        res.write('Enter valid number');
-    }
-    else {
-        res.write('sum of ' + a + " + " + b + " = " + (a+b));
-    }
-
-    res.end();    
-})
-
-app.use('/calc', lg,  (req, res) => {
-    const filePath = path.join(__dirname, 'public/add.html');
-    res.sendFile(filePath);
-})
-
-app.use('/', lg, (req, res) => {
-    const filePath = path.join(__dirname, 'public/index.html');
-    res.sendFile(filePath);
-})
-
-
-app.listen(5000);
+app.listen(5050);
