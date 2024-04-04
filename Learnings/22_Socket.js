3f5d694d74e3deb3041f2bc804db74abb78a0921
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-04 10:40:28 +0530

    Connect using socket .io

---
+++ Learnings/22_Socket.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/" + "index.html");
+	// res.send('Hello I am in Socket.io')
+});
+io.on("connection", function (socket) {
+	console.log("A user connected");
+	socket.on("disconnect", function () {
+		console.log("A user disconnected");
+	});
+});
+http.listen(2000, function () {
+	console.log("listening on localhost:2000");
+});
+
+/* 
+index.html: 
+
+<!DOCTYPE html> 
+<html> 
+   <head> 
+      <title>Hello world</title> 
+   </head> 
+   <script src = "/socket.io/socket.io.js"></script> 
+      <script> 
+      var socket = io(); 
+   </script> 
+   <body>Hello world</body> 
+</html>
+
+*/
