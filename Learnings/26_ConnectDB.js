442a72e067e9183767cfd66ec2802d67da2740b7
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-05 12:36:10 +0530

    CRUD

--- Learnings/26_ConnectDB.js
+++ Learnings/26_ConnectDB.js
@@ -1,8 +1,38 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://127.0.0.1:27017/Univ';
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://127.0.0.1:27017/Univ";
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
-    if (err) throw err;
-    console.log('DB Created');
-    db.close;
+// MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+// 	if (err) throw err;
+// 	console.log("DB Created");
+// 	db.close;
+// });
+
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+	if (err) throw err;
+	var dbo = db.db("Univ");
+	var myobj = [
+		{
+			name: "Manoj",
+			course: "Node js",
+			marks: 98,
+			Grade: "O",
+			Mode: "Regular",
+		},
+		{ name: "Dhoni", course: "Node js", marks: 98 },
+		{ name: "Anu", course: "React js", marks: 98 },
+		{ name: "Ram", course: "HTML", marks: 98 },
+		{ name: "Tej", course: "Angular js", marks: 98 },
+		{
+			name: "Manoj",
+			course: "Node js",
+			marks: 98,
+			Grade: "O",
+			Mode: "Regular",
+		},
+	];
+	dbo.collection("Student_data").insertMany(myobj, function (err, data) {
+		if (err) throw err;
+		console.log("Number of documents inserted: " + data.insertedCount);
+		db.close();
+	});
 });
